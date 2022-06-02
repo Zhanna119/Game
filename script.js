0d1af1b41d67f8b80150a77957bc9c49b9d1f94d
Zhanna119 <104631688+Zhanna119@users.noreply.github.com>
2022-06-02 13:13:16 +0300

    10.2

--- script.js
+++ script.js
@@ -1,31 +1,108 @@
 'use strict';
 
-document.addEventListener('DOMContentLoaded', function(){
+const scoreDisplay =  document.querySelector('.score');
+const resultDisplay = document.querySelector('.result');
+const colorCell = [
+    '#afa192', 
+    '#eee4da', 
+    '#ede0c8', 
+    '#f2b179', 
+    '#ffcea4', 
+    '#e8c064', 
+    '#ffab6e', 
+    '#fd9982', 
+    '#ead79c', 
+    '#76daff', 
+    '#beeaa5', 
+    '#d7d4f0',
+];
+
+const newCell = document.createElement('div');
+    const grid = document.querySelector('.grid');
+    const fragment = document.createDocumentFragment();
+
+    for(let i = 0; i <= 15; i++) {
+        
+        var div = document.createElement('div');
+        fragment.appendChild(div);
+        newCell.className = 'cell';
+        div.classList.add('cell');
+    }
+    grid.appendChild(fragment);
     
-    let grid = document.querySelector('.grid');
-        for(let i = 1; i<=16; i++){
-            let newCell = document.createElement('div');
-            newCell.className = 'cell';
-            grid.append(newCell);
+
+document.addEventListener('keyup', clickControl);
+
+function clickControl(event) {
+    if (event.key === 'ArrowUp') {
+        console.log('Вы нажали клавишу вверх!')
+    } else if (event.key === 'ArrowLeft') {
+        console.log('Вы нажали клавишу влево!')
+    } else if (event.key === 'ArrowRight') {
+        console.log('Вы нажали клавишу вправо!')
+    } else {
+        console.log('Вы нажали клавишу вниз!');
         }
+}
 
-})
+class GameManager{
+    constructor(isGameOver, score, board) {
+        this.isGameOver = false;
+        this.score = 0;
+        this.board = null;
+    }
+    init() {
+        this.board = new Board();
+            document.addEventListener('keyup', clickControl);
+    }
+}
 
-/* создать массив с пустыми клетками
+const start = new GameManager();
+start.init();
+//checkIsGameOver();
 
-создать переменную с числом 2
+class Board {
+    constructor(widthBoard, squares, wrapper) {
+        this.widthBoard = 4;
+        this.squares = [];
+        this.wrapper = document.querySelector('.grid');
+    }
 
-создать функцию, которая будет рандомно показывать число 2 в 
-одной из клеток при каждом нажатии по клавишам
+    init() {
+        const fragment = document.createDocumentFragment();
+    
+        for (let i = 0; i < this.width * this.width; i++) {
+            const square = document.createElement('div');
+            square.innerHTML = '';
+            square.className = 'grid-cell ';
+    
+            fragment.appendChild(square);
+            squares.push(square);
+        }
+        this.wrapper.appendChild(fragment);
+    }
+}
 
-создать функцию, которая будет сдвигать все цифры на поле 
-в ту сторону, в которую игрок нажал клавишу и которая будет 
-складывать все одинаковые цифры
+//generateNewCell();
+/*addColours() {
+    
+};*/
 
-1если происходит нажатие клавиши и не поменялся номинал - хода не совершается
+class Cell {
+    constructor(value, dom) {
+        this.value = '';
+        this.dom = null;
+    }
+    getValue () {
+        return this.value; 
+    }
 
-если заняты все клеточки и при нажатии нет одинаковых чисел - игра заканчивается*/
+    setValue() {
 
+    }
 
+    getNewElement() {
 
+    }
+}
 
